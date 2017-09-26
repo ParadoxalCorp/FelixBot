561db8172aad5fa1cb0d5f433bc1dfb2c298d942
ParadoxOrigins <funxgame1@gmail.com>
2017-09-26 08:34:23 +0200

    Update 2.3.0 - Final ~~countdown~~ fix
    Fixed both error catch of guildMemberAdd and guildMemberRemove

--- events/guildMemberAdd.js
+++ events/guildMemberAdd.js
@@ -6,15 +6,11 @@ module.exports = async(client, member) => {
         guildEntry.onEvent.guildMemberAdd.onJoinRole = existingRoles; //Update the
         client.guildData.set(member.guild.id, guildEntry); //guild entry
     }
-    if (guildEntry.onEvent.guildMemberAdd.greetings.message || guildEntry.onEvent.guildMemberAdd.greetings.embed) {
+    if (guildEntry.onEvent.guildMemberAdd.greetings.message && guildEntry.onEvent.guildMemberAdd.greetings.enabled) {
         let message = guildEntry.onEvent.guildMemberAdd.greetings.message || guildEntry.onEvent.guildMemberAdd.greetings.embed; //Whether the message to send is an embed or not
         let method = member.user;
         //---------------------------------------Replace all instances of %USER%, %USERNAME% and %GUILD%--------------------------------------
-        if (typeof message === "string") {
-            message = message.replace(/\%USER\%/gim, `<@${member.user.id}>`).replace(/\%USERNAME\%/gim, `${member.user.username}`).replace(/\%USERTAG%/gim, `${member.user.tag}`).replace(/\%GUILD\%/gim, `${member.guild.name}`);
-        } else {
-            message.embed.description = message.embed.description.replace(/\%USER\%/gim, `<@${member.user.id}>`).replace(/\%USERNAME\%/gim, `${member.user.username}`).replace(/\%USERTAG%/gim, `${member.user.tag}`).replace(/\%GUILD\%/gim, `${member.guild.name}`);
-        }
+        message = message.replace(/\%USER\%/gim, `<@${member.user.id}>`).replace(/\%USERNAME\%/gim, `${member.user.username}`).replace(/\%USERTAG%/gim, `${member.user.tag}`).replace(/\%GUILD\%/gim, `${member.guild.name}`);
         //---------------------------------------------------------Greets------------------------------------------------------------
         if (guildEntry.onEvent.guildMemberAdd.greetings.channel && !guildEntry.onEvent.guildMemberAdd.greetings.dm) method = member.guild.channels.get(guildEntry.onEvent.guildMemberAdd.greetings.channel);
         await method.send(message).catch(err => {
