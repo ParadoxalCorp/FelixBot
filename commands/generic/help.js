201987a005736a6d4b85b7d9f46334cfc14336a7
ParadoxOrigins <funxgame1@gmail.com>
2017-11-29 16:16:06 +0100

    Update 3.0.0 - Disabled modules list
    also added the -all filter to the help and apparently succeed to ignore unknow message errors, missing permissions errors soonTM

--- commands/generic/help.js
+++ commands/generic/help.js
@@ -6,7 +6,7 @@ class Help {
             name: 'help',
             description: 'Display a list of all the available commands or the detailed help of a specific command',
             usage: 'help ping',
-            detailedUsage: '`{prefix}help -dm` The `-dm` parameter will send the help in dm instead of this channel\n`{prefix}help -allowed` Will only display commands that you are allowed to use'
+            detailedUsage: '`{prefix}help -dm` The `-dm` parameter will send the help in dm instead of this channel\n`{prefix}help -allowed` Will only display commands that you are allowed to use\n`{prefix}help -all` Show all the commands, without hiding the disabled ones'
         }
         this.conf = {
             aliases: ['halp', 'h']
@@ -20,8 +20,9 @@ class Help {
                 let categories = await fs.readdir(`./commands`);
                 const dm = new RegExp(/-dm/gim).test(message.content);
                 const allowed = new RegExp(/-allowed/gim).test(message.content);
+                const all = new RegExp(/-all/gim).test(message.content);
                 if (!client.config.admins.includes(message.author.id)) categories = categories.filter(c => c !== 'admin'); //If felix admin then show admin commands
-                if (message.guild && !message.guild.members.get(message.author.id).hasPermission("administrator") && !dm) {
+                if (message.guild && !message.guild.members.get(message.author.id).hasPermission("administrator") && !dm && !all) {
                     categories = categories.filter(c => !client.guildData.get(message.guild.id).generalSettings.disabledModules.includes(`${c}*`));
                 }
                 let overallHelp = new String();
@@ -29,11 +30,11 @@ class Help {
                     let categoryCommands = client.commands.filter(c => c.help.category == categories[i]);
                     if (allowed && !message.guild) categoryCommands = categoryCommands.filter(c => c.conf.guildOnly ? false : true);
                     else if (allowed && message.guild) categoryCommands = categoryCommands.filter(c => require(`../../util/helpers/permissionsChecker.js`)(client, message, c));
-                    overallHelp += `**${categories[i]}** =>` + categoryCommands.filter(c => message.guild ? (!client.guildData.get(message.guild.id).generalSettings.disabledModules.includes(c.help.name) || message.guild.members.get(message.author.id).hasPermission("administrator")) : true).map(c => `\`${c.help.name}\` `) + "\n\n";
+                    overallHelp += `**${categories[i]}** =>` + categoryCommands.filter(c => message.guild ? (!client.guildData.get(message.guild.id).generalSettings.disabledModules.includes(c.help.name) || all || message.guild.members.get(message.author.id).hasPermission("administrator")) : true).map(c => `\`${c.help.name}\` `) + "\n\n";
                 }
                 overallHelp = overallHelp.replace(/undefined/gim, ""); //To remove the "undefined"
                 //--------------------------------------------------------Return overall help if no args-----------------------------------------------------
-                if (args.length === 0 || dm || allowed) {
+                if (args.length === 0 || dm || allowed || all) {
                     if (dm) return resolve(await message.author.createMessage(`Here's the list of all commands categories and their respective commands. Use \`${prefix}help command name\` to see the detailed description of a command\n\n` + overallHelp + `\n\n**Tips**\nYou can run up to 3 commands at once using \`&&\`, so for example: \`${prefix}ping && ${prefix}awoo\`\nIf you made a typo while writing a command, as long as its your last message, you can edit it: Felix will run it`));
                     else return resolve(await message.channel.createMessage(`Here's the list of all commands categories and their respective commands. Use \`${prefix}help command name\` to see the detailed description of a command\n\n` + overallHelp + `\n\n**Tips**\nYou can run up to 3 commands at once using \`&&\`, so for example: \`${prefix}ping && ${prefix}awoo\`\nIf you made a typo while writing a command, as long as its your last message, you can edit it: Felix will run it`));
                 };
