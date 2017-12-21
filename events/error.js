45d51a430507e14c01bd7e298bbe9345e284cf75
ParadoxOrigins <funxgame1@gmail.com>
2017-12-21 16:27:27 +0100

    do something even smarter
    fully relying on sentry is not the smartest thing to do :eyes:

--- events/error.js
+++ events/error.js
@@ -5,6 +5,7 @@ module.exports = async(client, err, message) => {
         let Raven = require("raven");
         Raven.config(client.config.raven).install();
         Raven.captureException(err, { triggerMessage: message ? message.content : undefined });
+        logger.log(`${err} ${err.stack} | triggerMessage: ${message ? message.content : undefined}`, `error`);
     } else logger.log(`${err} ${err.stack} | triggerMessage: ${message ? message.content : undefined}`, `error`);
     if (message) {
         try {
