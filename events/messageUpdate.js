ddc462e6f43e8c62824ddacdac4b5eacc56032d8
ParadoxOrigins <funxgame1@gmail.com>
2017-12-27 13:26:01 +0100

    update 3.0.0 - fixes episode 1

--- events/messageUpdate.js
+++ events/messageUpdate.js
@@ -1,4 +1,4 @@
 module.exports = async(client, message, oldMessage) => {
     //Basically emit a new message if the old message is cached(meaning its recent enough) 
-    if (message.channel.messages.has(message.id) && message.author.lastMessageID === message.id) client.emit("messageCreate", message);
+    if (message.channel.messages.has(message.id) && message.channel.messages.get(message.id).author.lastMessageID === message.id) client.emit("messageCreate", message);
 }
