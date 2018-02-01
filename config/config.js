53ba047018541e95d9906665502353c1c33831fb
ParadoxOrigins <ParadoxOrigins@users.noreply.github.com>
2018-02-01 18:10:36 +0100

    Update to the actual default prefix

--- config/config.js
+++ config/config.js
@@ -2,7 +2,7 @@ module.exports = {
     //An array of trustable peoples IDs, put yours as well since in most case this is checked rather than the ownerID
     admins: ["TRUSTABLE_PEOPLE_ID_1", "TRUSTABLE_PEOPLE_ID_2", "WHY_NOT_A_THIRD"],
     //The default prefix, this will be one by default on each new server and in dm
-    prefix: "uwu.",
+    prefix: "f!",
     //Your ID
     ownerID: "YOUR_ID",
     //API key for different bot lists, in a selfhost case, leave that false
